e5808e3859607b929138f3425ab5cb6bbeddd6f9
rosario <sarhus@gmail.com>
2013-07-13 11:56:19 +0100

    adding support for multi currency

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -4,13 +4,16 @@ Spree::Variant.class_eval do
   
   attr_accessible :option_values
   
-  def to_hash
-    actual_price  = self.price
+  def to_hash(currency)
+    # actual_price  = self.price
     #actual_price += Calculator::Vat.calculate_tax_on(self) if Spree::Config[:show_price_inc_vat]
+    actual_price = price_in(currency)
+    
+    
     { 
       :id    => self.id, 
       # :count => 1, # Rosario self.count_on_hand is not available anymore on Spree 2.0, 
-      :price => number_to_currency(actual_price)
+      :price => actual_price.display_price.to_s
     }
   end
     
